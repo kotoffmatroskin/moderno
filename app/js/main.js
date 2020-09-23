57a555f9ae233e54f0aec971c6db59d732cd0082
kotoffmatroskin <kotoffmatroskin@gmail.com>
2020-09-23 20:41:31 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,34 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
-    arrows: false
-  })
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+    ]
+  });
 
   $(".js-range-slider").ionRangeSlider({
     type: "double",
